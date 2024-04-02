8a001a0ea41cc20bf915ed2e64e3bc0c9da13423
almustanir <olajidemubarak561@gmail.com>
2024-04-02 10:24:39 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ullam accusantium quam excepturi, iure non molestiae. Mollitia qui eligendi laborum quae praesentium. Quae nulla eligendi tenetur, commodi enim minus nihil.</main>
+  );
+}
